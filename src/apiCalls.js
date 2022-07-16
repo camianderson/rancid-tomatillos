609b876ff5f3eca85b83a09c184954476adfcf46
Cami Anderson <camidantas@icloud.com>
2022-07-16 11:05:05 -0400

    Add error handling for router

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,23 +1,27 @@
+
 export let getMovieData = () => {
-  return fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies").then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error("Oops, something went wrong!");
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => {
+      if(!response.ok){
+        throw new Error('Oops, something went wrong!')
       } else {
-        return response.json();
+       return response.json()
       }
-    }
-  );
-};
+    })
+}
 
 export let getMovieDetails = (id) => {
-  return fetch(
-    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error("Oops, something went wrong!");
-    } else {
-      return response.json();
-    }
-  });
-};
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    .then(response => {
+      if(!response.ok){
+        throw new Error('Oops, something went wrong!')
+      } else {
+       return response.json()
+      }
+    })
+}
+
+
+
+
+
